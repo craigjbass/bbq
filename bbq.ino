9d73ab3b6ca7e88aac2b3a3b3af4723643ed4146
Craig J. Bass <craig@madetech.co.uk>
2017-05-01 19:03:39 +0100

    Add cleaned up version of arduino sketch

---
+++ bbq.ino
@@ -0,0 +1,51 @@
+#include "max6675.h"
+#include <AFMotor.h>
+
+int thermoDO = 5;
+int thermoCS = 6;
+int thermoCLK = 9;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+AF_DCMotor motor(1, MOTOR12_1KHZ); // create motor #2, 64KHz pwm
+
+void setup() {
+  Serial.begin(9600);
+  motor.setSpeed(255);
+  delay(1000);
+  startMotor();
+}
+
+void loop() {
+   double currentTemp = thermocouple.readCelsius();
+  
+   beforeChangeMotor(currentTemp);
+   if(isAtCookingTemperature(currentTemp)) {
+    stopMotor();
+   } else if(isTooCold(currentTemp)) {
+    startMotor();
+   }
+
+   delay(1000);
+}
+
+void startMotor() {
+  motor.run(BACKWARD);
+}
+
+void stopMotor() {
+  motor.run(RELEASE);
+}
+
+bool isTooCold(double temperature) {
+  return temperature < 110;
+}
+
+bool isAtCookingTemperature(double temperature) {
+  return temperature >= 110;
+}
+
+void beforeChangeMotor(double temperature) {
+  Serial.print("C = ");
+  Serial.println(temperature);
+}
+
